59206455dbfd9f2a5abc3f097cfdba0c5c2afd9b
daehyuk1231 <daehyuk1231@naver.com>
2025-02-21 22:18:33 +0900

    init : project set

--- src/main/java/com/meossamos/smore/domain/main/main/controller/MainController.java
+++ src/main/java/com/meossamos/smore/domain/main/main/controller/MainController.java
@@ -11,7 +11,7 @@ import org.springframework.web.bind.annotation.RestController;
 @RestController
 @RequestMapping("/api/v1/articles")
 @RequiredArgsConstructor
-public class ApiV1MainController {
+public class MainController {
     private final MainService mainService;
 
     @PostMapping("/test")
