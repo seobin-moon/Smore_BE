c15fca4658ffc16a4b778efd3aa899d2117b2874
seokyoung <nsy7160@gmail.com>
2025-02-26 17:40:35 +0900

    feat: rsData

--- src/main/java/com/meossamos/smore/domain/study/schedule/controller/ApiV1StudyScheduleController.java
+++ src/main/java/com/meossamos/smore/domain/study/schedule/controller/ApiV1StudyScheduleController.java
@@ -3,6 +3,7 @@ package com.meossamos.smore.domain.study.schedule.controller;
 import com.meossamos.smore.domain.study.schedule.dto.StudyScheduleDto;
 import com.meossamos.smore.domain.study.schedule.entity.StudySchedule;
 import com.meossamos.smore.domain.study.schedule.repository.StudyScheduleRepository;
+import com.meossamos.smore.global.rsData.RsData;
 import lombok.RequiredArgsConstructor;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
@@ -20,12 +21,19 @@ public class ApiV1StudyScheduleController {
 
     // 다건 조회
     @GetMapping("/{study_id}/schedules")
-    public List<StudyScheduleDto> getSchedules(@PathVariable Long study_id){
+    public RsData<List<StudyScheduleDto>> getSchedules(@PathVariable Long study_id){
 //        List<Schedule> scheduleList =
         List<StudySchedule> StudyScheduleList =  studyScheduleRepository.findByStudy_Id(study_id);
-        List<StudyScheduleDto> StudyScheduleDtoList = StudyScheduleList.stream()
+        List<StudyScheduleDto> studyScheduleDtoList = StudyScheduleList.stream()
                 .map(StudyScheduleDto::new).toList();
-        return StudyScheduleDtoList;
+        studyScheduleDtoList.forEach(studyScheduleDto ->
+                System.out.println(studyScheduleDto.getContent())
+                );
+        return new RsData<>(
+                "200",
+        "스케줄 다건 조회",
+                studyScheduleDtoList
+                );
     }
     // 상세 조회
 
