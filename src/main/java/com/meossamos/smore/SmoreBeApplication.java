dd830b1f32a2401f6bddeba897d00cb5df67241b
JeinChoi <cym2009@naver.com>
2025-02-28 15:09:11 +0900

    fix : REcruitmentArticleHashTagDocRepository 관련 파일 삭제

--- src/main/java/com/meossamos/smore/SmoreBeApplication.java
+++ src/main/java/com/meossamos/smore/SmoreBeApplication.java
@@ -3,11 +3,14 @@ package com.meossamos.smore;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.boot.autoconfigure.domain.EntityScan;
+import org.springframework.data.elasticsearch.repository.config.EnableElasticsearchRepositories;
 import org.springframework.data.jpa.repository.config.EnableJpaAuditing;
 
 @EnableJpaAuditing
 @SpringBootApplication
 @EntityScan(basePackages = {"com.meossamos.smore.domain"})
+//@EnableElasticsearchRepositories(basePackages = "com.meossamos.smore.domain.article.recruitmentArticleHashTag.repository")
+
 public class SmoreBeApplication {
 
     public static void main(String[] args) {
