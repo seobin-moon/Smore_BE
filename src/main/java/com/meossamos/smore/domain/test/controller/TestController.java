529ba5f7386778390dc11be52f47bc4501d7112b
shjk0531 <shinjk0531@gmail.com>
2025-03-08 21:30:57 +0900

    feature: s3 file upload

--- src/main/java/com/meossamos/smore/domain/test/controller/TestController.java
+++ src/main/java/com/meossamos/smore/domain/test/controller/TestController.java
@@ -1,6 +1,8 @@
 package com.meossamos.smore.domain.test.controller;
 
+import com.meossamos.smore.global.s3.S3Service;
 import lombok.RequiredArgsConstructor;
+import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 import org.springframework.web.multipart.MultipartFile;
 import software.amazon.awssdk.core.sync.RequestBody;
@@ -10,77 +12,38 @@ import software.amazon.awssdk.services.s3.model.DeleteObjectRequest;
 import software.amazon.awssdk.services.s3.model.PutObjectRequest;
 
 import java.io.IOException;
+import java.util.HashMap;
 import java.util.List;
+import java.util.Map;
 import java.util.stream.Collectors;
 
 @RestController
 @RequiredArgsConstructor
-@RequestMapping("/test")
+@RequestMapping("/api/v1/s3")
 public class TestController {
-    private final S3Client s3Client;
-    private static final String PUBLIC_BUCKET_NAME = "smore-dev-bucket-public-1";
-    private static final String PRIVATE_BUCKET_NAME = "smore-dev-bucket-private-1";
-    private static final String REGION = "ap-northeast-2";
-    private static final String IMG_DIR_NAME = "img1";
+    // S3 관련 비즈니스 로직을 처리하는 서비스 주입
+    private final S3Service s3Service;
 
-    public static String getS3FileUrl(String fileName) {
-        return "https://" + PUBLIC_BUCKET_NAME + ".s3." + REGION + ".amazonaws.com/" + fileName;
-    }
+    /**
+     * 프리사인 URL을 생성하여 반환하는 엔드포인트
+     *
+     * 예시 요청: GET /api/s3/presign?fileName=myimage.png&contentType=image/png
+     *
+     * @param fileName    업로드할 파일의 이름
+     * @param contentType 업로드할 파일의 MIME 타입
+     * @return 프리사인 URL을 포함한 JSON 응답
+     */
+    @GetMapping("/presign")
+    public ResponseEntity<Map<String, String>> getPresignedUrl(
+            @RequestParam("fileName") String fileName,
+            @RequestParam("contentType") String contentType
+    ) {
+        // 서비스 호출하여 프리사인 URL 생성
+        String presignedUrl = s3Service.generatePresignedUrl(fileName, contentType);
 
-    @GetMapping("/")
-    public List<String> home() {
-        List<Bucket> bucketList = s3Client.listBuckets().buckets();
-        return bucketList.stream().map(Bucket::name).collect(Collectors.toList());
-    }
-
-    @GetMapping("/upload")
-    public String upload() {
-        return """
-                <form action="/test/upload" method="post" enctype="multipart/form-data">
-                    <input type="file" name="file" accept="image/*">
-                    <input type="submit" value="Upload">
-                </form>
-                """;
-    }
-
-    @PostMapping("/upload")
-    @ResponseBody
-    public String handleFileUpload(MultipartFile file) throws IOException {
-        PutObjectRequest putObjectRequest = PutObjectRequest.builder()
-                .bucket(PUBLIC_BUCKET_NAME)
-                .key(IMG_DIR_NAME + "/" + file.getOriginalFilename())
-                .contentType(file.getContentType())
-                .build();
-
-        s3Client.putObject(putObjectRequest,
-                RequestBody.fromInputStream(file.getInputStream(), file.getSize()));
-
-        return"""
-                <img src="%s">
-                <hr>
-                <div>업로드 완료</div>
-                """.formatted(getS3FileUrl(IMG_DIR_NAME + "/" + file.getOriginalFilename()));
-    }
-
-    @GetMapping("/deleteFile")
-    public String showDeleteFile() {
-        return """
-                <form action="/test/deleteFile" method="post">
-                    <input type="text" name="fileName">
-                    <input type="submit" value="delete">
-                </form>
-                """;
-    }
-
-    @PostMapping("/deleteFile")
-    @ResponseBody
-    public String deleteFile(String fileName) {
-        DeleteObjectRequest deleteObjectRequest = DeleteObjectRequest.builder()
-                .bucket(PUBLIC_BUCKET_NAME)
-                .key(IMG_DIR_NAME + "/" + fileName)
-                .build();
-
-        s3Client.deleteObject(deleteObjectRequest);
-        return "파일이 삭제되었습니다.";
+        // 응답 결과를 Map으로 생성하여 반환
+        Map<String, String> response = new HashMap<>();
+        response.put("url", presignedUrl);
+        return ResponseEntity.ok(response);
     }
 }
